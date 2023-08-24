cb7605f8f566838a13aff4722b569e0346c5d839
balraaz15 <balraaz11@gmail.com>
2023-08-24 23:32:21 +0545

    chore: setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css';
+import { Button } from ".";
+import "./App.css";
 
 function App() {
-  return <>W-DESIGN</>;
+  return (
+    <>
+      <h1>W-DESIGN</h1>
+      <Button />
+    </>
+  );
 }
 
 export default App;
---
+++ src/w-design-core/atoms/Button/index.tsx
@@ -0,0 +1,7 @@
+export const Button = () => {
+  return (
+    <button className="bg-sky-500 cursor-pointer text-white px-4 py-3">
+      Simple Button
+    </button>
+  );
+};
