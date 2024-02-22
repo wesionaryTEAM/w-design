e8cdaa0506a245da66c27156a9bda1f816e0374a
Binabh <binabhdevkota@gmail.com>
2024-02-22 18:15:39 +0545

    modify ring width of textarea as per figma design

--- src/components/atoms/TextArea/index.tsx
+++ src/components/atoms/TextArea/index.tsx
@@ -11,7 +11,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-gray-500 disabled:cursor-not-allowed disabled:opacity-50 invalid:ring-red-500 invalid:ring-1",
+          "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-gray-500 disabled:cursor-not-allowed disabled:opacity-50 invalid:ring-red-500 invalid:ring-1",
           className
         )}
         ref={ref}
