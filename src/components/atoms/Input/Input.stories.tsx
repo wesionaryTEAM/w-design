e16e7b77bc8c4cd98258d9504b50bedfe9ae0b31
Binabh <binabhdevkota@gmail.com>
2024-02-22 18:12:53 +0545

    use single component for icons also instead of two

--- src/components/atoms/Input/Input.stories.tsx
+++ src/components/atoms/Input/Input.stories.tsx
@@ -52,3 +52,32 @@ export const Search: InputStory = {
     placeholder: 'Search Input',
   },
 };
+
+
+export const Default: InputStory = {
+  args: {
+    variant: "default",
+    disabled: false,
+    type: 'text',
+    placeholder: 'Text Input',
+  },
+}
+ export const WithTickInFront: InputStory = {
+  args: {
+    variant: "default",
+    disabled: false,
+    required: true,
+    type: 'text',
+    placeholder: 'Text Input',
+    prefixIcon: <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 6L9 17l-5-5 1.5-1.5L9 14.5 18.5 5 20 6z" fill="#000"/></svg>
+  },
+};
+ export const EmailWithTickInBack: InputStory = {
+  args: {
+    variant: "default",
+    disabled: false,
+    type: 'email',
+    placeholder: 'Email Input',
+    suffixIcon: <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 6L9 17l-5-5 1.5-1.5L9 14.5 18.5 5 20 6z" fill="#0f0"/></svg>
+  },
+};
--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -5,7 +5,7 @@ import * as React from "react"
 import { VariantProps, tv } from "tailwind-variants";
 
 const input = tv({
-  base: "flex w-full text-base rounded-md border border-input bg-transparent px-3 py-1 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-gray-500 disabled:cursor-not-allowed disabled:opacity-50  invalid:ring-red-500 invalid:ring-1",
+  base: "flex w-full text-base rounded-md border border-input bg-transparent px-3 py-1 shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-gray-500 disabled:cursor-not-allowed disabled:opacity-50  invalid:ring-red-500 invalid:ring-1",
   variants: {
     variant: {
       small: "h-9",
@@ -21,29 +21,13 @@ type InputVarinats = VariantProps<typeof input>;
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement>, InputVarinats {
   type: "text" | "password" | "email" | "number" | "search";
-}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, variant, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(input({ variant }), className)}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-
-export interface InputWithIconProps extends InputProps {
   prefixIcon?: React.ReactNode;
   suffixIcon?: React.ReactNode;
   prefixPadding?: string;
   suffixPadding?: string;
 }
 
-const InputWithIcon = React.forwardRef<HTMLInputElement, InputWithIconProps>(
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, prefixIcon, prefixPadding, suffixPadding, suffixIcon, variant, type, ...props }, ref) => {
     const inputPadding = `${prefixIcon ? prefixPadding ? prefixPadding : "pl-10" : ""} ${suffixIcon ? suffixPadding ? suffixPadding : "pr-10" : ""}`;
     return (
@@ -66,6 +50,5 @@ const InputWithIcon = React.forwardRef<HTMLInputElement, InputWithIconProps>(
 )
 
 Input.displayName = "Input"
-InputWithIcon.displayName = "InputWithIcon"
 
-export { Input, InputWithIcon }
+export { Input }
