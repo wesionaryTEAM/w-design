0ae2d9731b8e59ff4aa7be570faa06d2b4f3044a
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-03-04 00:03:57 +0545

    Add size options to Loader component

--- src/components/atoms/Loader/index.tsx
+++ src/components/atoms/Loader/index.tsx
@@ -7,6 +7,12 @@ const loader = tv({
       secondary: "text-green-500",
       default: "text-gray-500",
     },
+    size: {
+      small: "h-8 w-8",
+      medium: "h-12 w-12",
+      large: "h-24 w-24",
+      default: "h-10 w-10",
+    },
   },
 });
 
