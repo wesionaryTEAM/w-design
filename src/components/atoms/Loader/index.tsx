786ec1c3229c3ce9c3ca893844ce6fbd8cd1281b
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-03-04 00:03:16 +0545

    Add variant options to Loader component

--- src/components/atoms/Loader/index.tsx
+++ src/components/atoms/Loader/index.tsx
@@ -1,7 +1,13 @@
 import { VariantProps, tv } from "tailwind-variants";
 
 const loader = tv({
-  variants: {},
+  variants: {
+    variant: {
+      primary: "text-blue-500",
+      secondary: "text-green-500",
+      default: "text-gray-500",
+    },
+  },
 });
 
 export type LoaderProps = VariantProps<typeof loader> & {
