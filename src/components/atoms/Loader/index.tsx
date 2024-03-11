55b5850586a383a19f27d25fbb5437f0f58fa0a7
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-03-11 23:33:07 +0545

    Update Loader component styles

--- src/components/atoms/Loader/index.tsx
+++ src/components/atoms/Loader/index.tsx
@@ -5,15 +5,13 @@ const loader = tv({
   base: "flex items-center justify-center",
   variants: {
     color: {
-      primary: "text-blue-500",
+      primary: "text-gray-500",
       secondary: "text-green-500",
-      default: "text-gray-500",
     },
     size: {
       small: "h-14 w-14",
       medium: "h-16 w-16",
       large: "h-18 w-18",
-      default: "h-12 w-12",
     },
     messageSize: {
       small: "text-sm",
@@ -27,8 +25,8 @@ const loader = tv({
     },
   },
   defaultVariants: {
-    color: "default",
-    size: "default",
+    color: "primary",
+    size: "medium",
     messageSize: "default",
     direction: "clockwise",
   },
@@ -40,22 +38,20 @@ export type LoaderProps = VariantProps<typeof loader> & {
 };
 
 export const Loader: React.FC<LoaderProps> = ({
-  color = "default",
-  size = "default",
+  color,
+  size,
   message,
-  direction = "clockwise",
-  messageSize = "default",
+  direction,
+  messageSize,
 }) => {
-  const variantClass = loader.variants.color[color];
-  const spinDirection = loader.variants.direction[direction];
-  const sizeClass = loader.variants.size[size];
-  const messageSizeClass = loader.variants.messageSize[messageSize];
-
   return (
-    <div role='status' className={cn("flex  flex-col items-center", sizeClass)}>
+    <div
+      role='status'
+      className={cn("flex  flex-col items-center", loader({ size }))}
+    >
       <svg
         aria-hidden='true'
-        className={cn("fill-current", variantClass, spinDirection)}
+        className={cn("fill-current", loader({ color, size, direction }))}
         viewBox='0 0 50 50'
         xmlns='http://www.w3.org/2000/svg'
       >
@@ -72,7 +68,10 @@ export const Loader: React.FC<LoaderProps> = ({
       </svg>
       {message && (
         <span
-          className={cn("flex items-center justify-center", messageSizeClass)}
+          className={cn(
+            "flex items-center justify-center",
+            loader({ messageSize })
+          )}
         >
           {message}
         </span>
