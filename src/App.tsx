c905875a583217bfdd682b55197ffaa8e55ba702
roshan.saud <rosansaud5@gmail.com>
2024-03-13 13:40:38 +0545

    feat: select component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { Button } from "@/components/atoms";
+import {
+  Button,
+  Input,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Textarea,
+} from "@/components/atoms";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>W-DESIGN</h1>
-      <Button  name="Hi There" />
+      <Button></Button>
+      <Input type='email' />
+      <Select>
+        <SelectTrigger className='w-[400px]'>
+          <SelectValue placeholder='Theme' />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value='light'>Light</SelectItem>
+          <SelectItem value='dark'>Dark</SelectItem>
+          <SelectItem value='system'>System</SelectItem>
+        </SelectContent>
+      </Select>
+      <Textarea />
     </>
   );
 }
