edb169279357b8b0d5804461fdc909abc11e2577
Dibesh Raj Subedi <dibeshrsubedi@gmail.com>
2024-03-18 17:29:12 +0545

    Add decorators and render function to Shimmer stories

--- src/components/atoms/Shimmer/Shimmer.stories.tsx
+++ src/components/atoms/Shimmer/Shimmer.stories.tsx
@@ -3,38 +3,61 @@ import { Shimmer } from '.';
 
 const meta: Meta<typeof Shimmer> = {
     title: 'Design System/Atoms/Shimmer',
+    decorators: [
+        (Story) => (
+            <div>
+                <p className='m-2 font-bold'>📝 Note: This component is an example only so please try playing with the props and customize as per need.</p>
+                <div className='p-4'>
+                    <Story />
+                </div>
+            </div>
+        )
+    ],
     component: Shimmer,
     tags: ['autodocs'],
     argTypes: {
-        className: {
-            control: {
-                type: 'text',
-            },
-            description: 'Additional classes to add to the shimmer',
+        variant: {
+            options: ['default', 'primary', 'secondary'],
+            defaultValue: 'default',
+            control: { type: 'radio' }
         },
+        animate: {
+            control: { type: 'boolean' },
+            defaultValue: true,
+            description: "Sets The Shimmer Animation On/Off"
+        },
+        width: {
+            table: {
+                disable: true
+            }
+        },
+        className: {
+            table: {
+                disable: true
+            }
+        }
+    },
+    render: ({ variant, animate }) => {
+        return (
+            <Shimmer className='flex gap-2' variant={variant} animate={animate}>
+                <Shimmer.Circle height='xxs' width='xxs' variant={variant} className='mr-2' animate={animate} />
+                <Shimmer.Box variant={variant} animate={animate} />
+                <Shimmer className='flex flex-col gap-2 mt-8' variant={variant} animate={animate}>
+                    <Shimmer.Line width='lg' variant={variant} animate={animate} />
+                    <Shimmer.Line width='xl' variant={variant} animate={animate} />
+                    <Shimmer.Line width='xl' variant={variant} animate={animate} />
+                </Shimmer>
+            </Shimmer>
+        )
     }
 };
 
 export default meta;
+type Example = StoryObj<typeof Shimmer>;
 
-type Story = StoryObj<typeof Shimmer>;
-
-export const Default: Story = {
+export const Default: Example = {
     args: {
-        className: 'w-[100px] h-[100px] bg-slate-100',
-    },
+        variant: 'default',
+        animate: true
+    }
 };
-
-export const Circle: Story = {
-    args: {
-        className: `${Default.args?.className} rounded-full`,
-    },
-};
-
-
-export const Line: Story = {
-    args: {
-        className: `w-[100px] h-[10px] bg-slate-100  rounded-md`,
-    },
-};
-
