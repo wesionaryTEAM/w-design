0c3e75c7f13173c192563985de55d08baacbbc30
Rodan Ramdam <rodan.ramdam@gmail.com>
2024-03-27 12:51:30 +0545

    update storybook example to have more accordion items

--- src/components/atoms/Accordion/Accordion.stories.tsx
+++ src/components/atoms/Accordion/Accordion.stories.tsx
@@ -17,11 +17,24 @@ type Story = StoryObj<typeof Accordion>;
 const AccordionTemplate: Story = {
   render: () => {
     return (
-      <Accordion type='single' collapsible>
+      <Accordion type='single' defaultValue='item-1' collapsible>
         <AccordionItem value='item-1'>
           <AccordionTrigger>Is it accessible?</AccordionTrigger>
           <AccordionContent>
             Yes. It adheres to the WAI-ARIA design pattern.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-2'>
+          <AccordionTrigger>Is it styled?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It comes with default styles that matches the other components'
+            aesthetic.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value='item-3'>
+          <AccordionTrigger>Is it animated?</AccordionTrigger>
+          <AccordionContent>
+            Yes. It's animated by default, but you can disable it if you prefer.
           </AccordionContent>
         </AccordionItem>
       </Accordion>
