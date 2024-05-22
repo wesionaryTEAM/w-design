776f4e64cc0d2a932b486e8f5015343b88a81469
Manoj Pudasaini <mpudasaini17@gmail.com>
2024-05-22 12:24:11 +0545

    styles: fix datepicker active button text color

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -41,8 +41,7 @@ function Calendar({
         day_range_end: "day-range-end",
         day_selected:
           "bg-slate-900 text-slate-50 hover:bg-slate-900 hover:text-slate-50 focus:bg-slate-900 focus:text-slate-50 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50 dark:hover:text-slate-900 dark:focus:bg-slate-50 dark:focus:text-slate-900",
-        day_today:
-          "bg-slate-100 text-slate-900 dark:bg-slate-800 dark:text-slate-50",
+        day_today: "bg-slate-100 dark:bg-slate-800 dark:text-slate-50",
         day_outside:
           "day-outside text-slate-500 opacity-50 aria-selected:bg-slate-100/50 aria-selected:text-slate-500 aria-selected:opacity-30 dark:text-slate-400 dark:aria-selected:bg-slate-800/50 dark:aria-selected:text-slate-400",
         day_disabled: "text-slate-500 opacity-50 dark:text-slate-400",
